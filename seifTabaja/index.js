61528f8c3dc2c7447b1724e9d1c3f54be15515fd
Seif Tabaja <saif_tabaja@hotmail.com>
2019-01-24 00:01:33 +0100

    commit

---
+++ seifTabaja/index.js
@@ -0,0 +1,26 @@
+let i = 0;
+let images = [];
+let time = 2000;
+images[0] = "images/1.jpg";
+images[1] = "images/2.jpg";
+images[2] = "images/3.jpg";
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout("changeImg()", time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
