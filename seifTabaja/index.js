15c46cb1481122bfeac08eff881c70365dd64103
Seif Tabaja <saif_tabaja@hotmail.com>
2019-02-18 02:10:28 +0100

    some fixed on seiftabaja and push pin map

--- seifTabaja/index.js
+++ seifTabaja/index.js
@@ -1,13 +1,18 @@
 let i = 0;
 let images = [];
-let time = 2000;
 images[0] = "images/1.JPG";
 images[1] = "images/2.JPG";
 images[2] = "images/3.JPG";
+let images2 = [];
+images2[0] = "images/pp1.JPG";
+images2[1] = "images/pp2.JPG";
+images2[2] = "images/pp3.JPG";
+let time = 2000;
 
 // Change Image
 function changeImg() {
   document.slide.src = images[i];
+  document.slide1.src = images2[i];
 
   // Check If Index Is Under Max
   if (i < images.length - 1) {
